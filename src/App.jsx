80b895acc186b9ea38661aa542730237be9339ea
Viraj Abeywardana <abeywardana21@gmail.com>
2024-02-05 00:11:33 +0530

    importing pages and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Profile from './pages/Profile'
+import About from './pages/About'
 
 export default function App() {
   return (
-    <div><h1 className='text-red-500'>App</h1></div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-out' element={<SignUp />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { ReactDOM } from 'react'
 
 export default function Profile() {
   return (
